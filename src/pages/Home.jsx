6bcb1f1e7534ea7bbce5d9aa2ea01532c974aadd
tasyanadia <tasyanadia2410@gmail.com>
2024-12-05 10:01:25 +0700

    fix: responsive home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,10 @@ import '../index.css';
 const Home = () => {
   return (
     <div 
-      // className="home-container"
       style={{
         height: "80vh",
       }}
-      className='d-flex flex-column justify-content-center align-items-center'
+      className='d-flex flex-column justify-content-center align-items-center text-center px-4'
     >
       <h1>Rock Paper Scissors Battle</h1>
       <p>Welcome to the Rock Paper Scissors game! Ready to play?</p>
